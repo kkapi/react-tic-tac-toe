6dd7a4ea73f8c37cb65a0f52505bbf67ce2fc194
kkapi <hZG2fik_j8@mail.ru>
2023-06-29 01:42:34 +0300

    setting up react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<button className='square'>X</button>
+			<button className='square'>X</button>
+			<button className='square'>X</button>			
+		</div>
+	);
+}
+
+export default App;
